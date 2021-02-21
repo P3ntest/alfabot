97241b38acc540cc7cf3375803c315a29432b905
RobinD95 <lopfi95@gmail.com>
2021-02-21 13:20:05 +0100

    deleteSubject command

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const ExportCmd = require('./commands/export');
 const sqlite3 = require('sqlite3').verbose();
 const { open } = require("sqlite");
 const DeleteTable = require('./commands/deleteTable');
+const DeleteSubject = require('./commands/deleteSubject');
 
 var db;
 (async () => {
@@ -94,6 +95,9 @@ client.on('message', async msg => {
                 currentCommands[msg.author.id].recieve(msg);
             } else if (msg.content.toLowerCase().startsWith("delete table")) {
                 currentCommands[msg.author.id] = new DeleteTable();
+                currentCommands[msg.author.id].recieve(msg);
+            } else if (msg.content.toLowerCase().startsWith("delete subject")) {
+                currentCommands[msg.author.id] = new DeleteSubject();
                 currentCommands[msg.author.id].recieve(msg);
             } else if (msg.content.trim().toLowerCase().startsWith("rename")) {
                 currentCommands[msg.author.id] = new RenameCmd(db);
--- src/commands/deleteSubject.js
+++ src/commands/deleteSubject.js
@@ -2,7 +2,7 @@ const { getActiveTimetable, canEdit, setActiveTimeTable, getNewId } = require(".
 const Discord = require('discord.js');
 const validUrl = require("valid-url");
 
-module.exports = class DeleteTable {
+module.exports = class DeleteSubject {
     constructor() {
         this.active = true;
         this.stage = 0;
@@ -10,9 +10,10 @@ module.exports = class DeleteTable {
     async recieve(msg) {
         switch (this.stage) {
             case 0:
-                const allTables = await global.db.all("SELECT * FROM timetables WHERE owner=?", [msg.author.id]);
-                if (!allTables) {
-                    msg.channel.send(new Discord.MessageEmbed().setColor("#ff2146").setDescription(":no_entry_sign:  **No timetable's found! Looks like you don't have any.**"));
+                this.activeTable = await global.db.get("SELECT timetable FROM activeTable WHERE client=?", [msg.author.id]);    
+                const allSubjects = await global.db.all("SELECT * FROM subjects WHERE timetable=?", [this.activeTable.timetable]);
+                if (allSubjects.length < 1) {
+                    msg.channel.send(new Discord.MessageEmbed().setColor("#ff2146").setDescription(":no_entry_sign:  **No subjects found! Looks like you don't have any.**"));
                     this.active = false;
                     return;
                 }
@@ -23,12 +24,12 @@ module.exports = class DeleteTable {
                     }
                     this.stage = 1;
                 } else {
-                    let embed = new Discord.MessageEmbed().setColor("#5cd1ff").setDescription(":question: **What timetable do you want to delete?**");
+                    let embed = new Discord.MessageEmbed().setColor("#5cd1ff").setDescription(":question: **What subject do you want to delete?**");
                     let listOfAll = "";
-                    allTables.forEach(row => {
+                    allSubjects.forEach(row => {
                         listOfAll += row.name + "\n";
                     });
-                    embed.addField("List of all your timetables:", listOfAll);
+                    embed.addField("List of all your subjects:", listOfAll);
                     msg.channel.send(embed);
                     this.stage = 1;
                 }
@@ -42,17 +43,17 @@ module.exports = class DeleteTable {
         }
     }
 
-    async deleteTable(msg, timetable) {
-        global.db.run("DELETE FROM timetables WHERE id=?", [timetable.id]);
+    async deleteSubject(msg, subject) {
+        global.db.run("DELETE FROM subjects WHERE name=?", [subject.name]);
             setActiveTimeTable(msg.author.id, "", null)
-            msg.channel.send(new Discord.MessageEmbed().setColor("#42f554").setDescription(":white_check_mark: **Deleted table.**"));
+            msg.channel.send(new Discord.MessageEmbed().setColor("#42f554").setDescription(":white_check_mark: **Deleted subject.**"));
             return true;
     }
 
     async confirm(msg) {
-        const timetable = await global.db.get("SELECT * FROM timetables WHERE LOWER (name) LIKE LOWER (?) AND owner = ?", [this.name, msg.author.id]);
-        if(!timetable) {
-            msg.channel.send(new Discord.MessageEmbed().setColor("#ff2146").setDescription(":no_entry_sign:  **Table not found, please try again. Use `cancel` to cancel.**"));
+        const subject = await global.db.get("SELECT * FROM subjects WHERE LOWER (name) LIKE LOWER (?) AND timetable = ?", [this.name, this.activeTable.timetable]);
+        if(!subject) {
+            msg.channel.send(new Discord.MessageEmbed().setColor("#ff2146").setDescription(":no_entry_sign:  **Subject not found, please try again. Use `cancel` to cancel.**"));
             return false;
         }
         else {
@@ -61,7 +62,7 @@ module.exports = class DeleteTable {
             await message.react("🟥");
             message.awaitReactions(() => true, {max: 1, time: 60000})
             .then(collected => {
-                if (collected.first().emoji.name == "✅") this.deleteTable(msg, timetable);
+                if (collected.first().emoji.name == "✅") this.deleteSubject(msg, subject);
                 else  msg.channel.send(new Discord.MessageEmbed().setColor("#f5b042").setDescription(":information_source: **Action has been cancelled.**"));
                 message.delete();
             });
